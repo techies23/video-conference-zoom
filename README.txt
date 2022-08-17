bec2d54fda0aad0c0afdf2aaecadfbf9d6810ee4
digamber <digamberpradhan89@gmail.com>
2022-08-17 10:54:58 +0545

    Bump version to 4.0.5 - minor fix

--- README.txt
+++ README.txt
@@ -4,7 +4,7 @@ Tags: zoom video conference, video conference, zoom, zoom video conferencing, we
 Donate link: https://www.paypal.com/donate?hosted_button_id=2UCQKR868M9WE
 Requires at least: 5.0
 Tested up to: 6.0
-Stable tag: 4.0.4
+Stable tag: 4.0.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -149,6 +149,9 @@ Yes, you should be registered in Zoom. Also, depending on the zoom account plan 
 9. Shortcode Output
 
 == Changelog ==
+
+= 4.0.5 August 17th, 2022 =
+* Fix - Minor - Meeting Host should not be editable even if post visibility is set to Private
 
 = 4.0.4 August 11th, 2022 =
 * Fixed: Import meetings not working for version 4.0 or greater.
--- video-conferencing-with-zoom-api.php
+++ video-conferencing-with-zoom-api.php
@@ -7,7 +7,7 @@
  * Plugin Name:       Video Conferencing with Zoom
  * Plugin URI:        https://wordpress.org/plugins/video-conferencing-with-zoom-api/
  * Description:       Video Conferencing with Zoom Meetings and Webinars plugin provides you with great functionality of managing Zoom meetings, Webinar scheduling options, and users directly from your WordPress dashboard.
- * Version:           4.0.4
+ * Version:           4.0.5
  * Author:            Deepen Bajracharya
  * Author URI:        https://www.imdpen.com
  * License:           GPL-2.0+
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 define( 'ZVC_PLUGIN_FILE', __FILE__ );
 define( 'ZVC_PLUGIN_SLUG', 'video-conferencing-zoom' );
-define( 'ZVC_PLUGIN_VERSION', '4.0.4' );
+define( 'ZVC_PLUGIN_VERSION', '4.0.5' );
 define( 'ZVC_ZOOM_WEBSDK_VERSION', '2.5.0' );
 define( 'ZVC_PLUGIN_AUTHOR', 'https://deepenbajracharya.com.np' );
 define( 'ZVC_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
