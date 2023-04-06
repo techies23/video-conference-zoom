db155ca4542f693e6b65dcd5fc7dd0d3388ffa1a
Deepen Bajracharya <dpen.connectify@gmail.com>
2023-04-06 11:04:10 +0545

    Lang update and zoom sdk package updated

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render', 'wp-url'), 'version' => '5024e7e2359754182646');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render', 'wp-url'), 'version' => 'd500c204002e672338e7');
