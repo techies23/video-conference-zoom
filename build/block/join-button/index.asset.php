f575b86b5eeed668032335d00753a9742ac84d61
digamber pradhan <digamberpradhan89@gmail.com>
2026-09-11 14:19:07 +0545

    Updated wp scripts package

--- build/block/join-button/index.asset.php
+++ build/block/join-button/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '524e71a35a551cdbc631');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6ca1768bd2daf856eaf1');
