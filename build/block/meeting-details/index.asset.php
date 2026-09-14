73d45072fdeed93fbf81c0dd4b2f91e97bf4a72e
digamber pradhan <digamberpradhan89@gmail.com>
2026-09-14 15:54:11 +0545

    Add space before and after :

--- build/block/meeting-details/index.asset.php
+++ build/block/meeting-details/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9ac2395c6b96279a0fe3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '93f2a6599350a82f38c8');
--- src/block/meeting-details/edit.js
+++ src/block/meeting-details/edit.js
@@ -1,31 +1,40 @@
-import { __ } from '@wordpress/i18n'
+import { __ } from '@wordpress/i18n';
 import {
   useBlockProps,
   useInnerBlocksProps,
   InspectorControls,
-} from '@wordpress/block-editor'
+} from '@wordpress/block-editor';
 
-// Import our shared meeting selector component
-import MeetingSourceSelector from '../components/MeetingSourceSelector'
+// Import shared meeting selector component
+import MeetingSourceSelector from '../components/MeetingSourceSelector';
 
-// Default template starter (optional baseline structure)
+// Nested template starter with child blocks pre-populated inside core/group
 const DEFAULT_TEMPLATE = [
   ['core/heading', { level: 3, content: __('Details', 'video-conferencing-with-zoom-api') }],
-  ['core/group', { layout: { type: 'flex', orientation: 'vertical' } }],
-]
+  [
+    'core/group',
+    { layout: { type: 'flex', orientation: 'vertical' } },
+    [
+      ['vczapi/meeting-details-topic', {}],
+      ['vczapi/meeting-details-hosted-by', {}],
+      ['vczapi/meeting-details-start-time', {}],
+      ['vczapi/meeting-details-duration', {}],
+      ['vczapi/meeting-details-timezone', {}],
+    ],
+  ],
+];
 
-export default function Edit ({ attributes, setAttributes }) {
-  const { sourceType, customMeetingId, selectedMeetingPostId } = attributes
+export default function Edit({ attributes, setAttributes }) {
+  const { sourceType, customMeetingId, selectedMeetingPostId } = attributes;
 
   const blockProps = useBlockProps({
     className: 'vczapi-meeting-details-container',
-  })
+  });
 
-  // Notice NO allowedBlocks restriction here
   const innerBlocksProps = useInnerBlocksProps(blockProps, {
     template: DEFAULT_TEMPLATE,
     templateInsertUpdatesSelection: false,
-  })
+  });
 
   return (
     <>
@@ -42,5 +51,5 @@ export default function Edit ({ attributes, setAttributes }) {
 
       <div {...innerBlocksProps} />
     </>
-  )
+  );
 }
