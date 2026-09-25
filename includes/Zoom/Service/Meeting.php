0ccefa4a5049a5b5d81971735098bd1c985ec825
digamber pradhan <digamberpradhan89@gmail.com>
2026-09-25 13:54:07 +0545

    Payload correctly build now

--- includes/Zoom/Service/Meeting.php
+++ includes/Zoom/Service/Meeting.php
@@ -170,10 +170,7 @@ class Meeting extends BaseService {
 		}
 
 		$prepared['body'] = apply_filters( 'vczapi_meetings_update_payload', $prepared['body'], $data );
-		file_put_contents( VCZAPI_PLUGIN_DIR_PATH . '/update-data.txt', print_r( $data, true ) );
-		file_put_contents( VCZAPI_PLUGIN_DIR_PATH . '/update-payload.txt', json_encode( $prepared['body'], JSON_PRETTY_PRINT ) );
 		$endpoint = ! empty( $prepared['query'] ) ? add_query_arg( $prepared['query'], $prepared['endpoint'] ) : $prepared['endpoint'];
-		return [];
 		$result = $this->client->request( $prepared['method'], $endpoint, $prepared['body'] );
 
 		if ( ! empty( $prepared['warnings'] ) ) {
